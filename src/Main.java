b1d9742a053d32fcda2bd29b404e51866c416c84
MikKonak <ant.konakova@gmail.com>
2023-08-01 07:57:43 -0700

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-
+// my file create
 public class Main {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
